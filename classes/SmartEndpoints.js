8f8f14504cd3c083e3c47475d37c09fa8222a141
Gabe Abrams <gabeabrams@gmail.com>
2018-09-25 13:58:08 -0400

    Added SmartEndpoints class and utilities folder

---
+++ classes/SmartEndpoints.js
@@ -0,0 +1,16 @@
+class SmartEndpoints {
+
+  /**
+   * Creates a new SmartEndpoints object
+   * @param {string} accessToken - An access token to add to all requests
+   * @param {object} cache - If true, uses a memory cache. If excluded, caching
+   *   is turned off. If an object, assumes the object is a custom cache manager
+   * @param {function} visitEndpoint - A function that sends a request to the
+   *   Canvas API. Should handle paging. 
+   * @param {string} canvasHost - The hostname to use when sending requests to
+   *   the Canvas API
+   */
+  constructor(options) {
+
+  }
+}
