c67fd71f9a872247ac3fb92caf61942c6d1ab7bd
Gabe Abrams <gabeabrams@gmail.com>
2018-10-01 15:59:03 -0400

    Added new utilities for including a value if it's a boolean, if it's a number, and if it's truthy

--- endpoints/helpers/utils.js
+++ endpoints/helpers/utils.js
@@ -4,4 +4,16 @@ module.exports = {
   includeIfTruthy: (value) => {
     return value || EXCLUDED_PARAM;
   },
+
+  includeIfBoolean: (value) => {
+    return ((value === true || value === false) ? value : EXCLUDED_PARAM);
+  },
+
+  includeIfNumber: (value) => {
+    return (!Number.isNaN(value) ? value : EXCLUDED_PARAM);
+  },
+
+  isTruthy: (value) => {
+    return !(!value);
+  },
 };
