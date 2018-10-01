96dd45c7dc571fe32f857a1106a652b64d37db47
Gabe Abrams <gabeabrams@gmail.com>
2018-10-01 16:00:55 -0400

    Added assignments, course, pages, sections to course category

--- endpoints/config.js
+++ endpoints/config.js
@@ -1,6 +1,10 @@
 // Course
 const courseApps = require('./course/apps.js');
+const courseAssignments = require('./course/assignments.js');
+const courseCourse = require('./course/course.js');
 const courseEnrollments = require('./course/enrollments.js');
+const coursePages = require('./course/pages.js');
+const courseSections = require('./course/sections.js');
 
 // User
 const userCourse = require('./user/course.js');
@@ -8,7 +12,11 @@ const userCourse = require('./user/course.js');
 module.exports = {
   course: [
     courseApps,
+    courseAssignments,
+    courseCourse,
     courseEnrollments,
+    coursePages,
+    courseSections,
   ],
   user: [
     userCourse,
