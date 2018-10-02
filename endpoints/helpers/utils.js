342ed0fd4a696c149a8563fa0dbc96a4c0491d25
Gabe Abrams <gabeabrams@gmail.com>
2018-10-02 17:05:05 -0400

    Added many more util functions

--- endpoints/helpers/utils.js
+++ endpoints/helpers/utils.js
@@ -1,3 +1,5 @@
+// wrapVisitEndpoint filters out all parameters with value equal to
+// EXCLUDED_PARAM. To exclude a parameter, just set its value to EXCLUDED_PARAM
 const EXCLUDED_PARAM = '-=EXCLUDED_PARAMETER=-';
 
 module.exports = {
@@ -5,12 +7,51 @@ module.exports = {
     return value || EXCLUDED_PARAM;
   },
 
-  includeIfBoolean: (value) => {
-    return ((value === true || value === false) ? value : EXCLUDED_PARAM);
+  includeTruthyElements: (arr) => {
+    // Exclude if this isn't an array
+    if (!arr || !Array.isArray(arr)) {
+      return EXCLUDED_PARAM;
+    }
+    // Filter non-truthy elements
+    return arr.filter((x) => {
+      return x;
+    });
+  },
+
+  includeTruthyElementsExcludeIfEmpty: (arr) => {
+    // Exclude if this isn't an array
+    if (!arr || !Array.isArray(arr)) {
+      return EXCLUDED_PARAM;
+    }
+    // Filter non-truthy elements
+    const onlyTruthy = arr.filter((x) => {
+      return x;
+    });
+    if (onlyTruthy.length === 0) {
+      // No truthy elements
+      return EXCLUDED_PARAM;
+    }
+    return onlyTruthy;
+  },
+
+  includeIfDate: (date) => {
+    if (date && date.toISOString) {
+      // This is a date object. Convert to ISO 8601 string
+      return date.toISOString();
+    }
+    // Exclude if no date included
+    return date || EXCLUDED_PARAM;
   },
 
   includeIfNumber: (value) => {
-    return (!Number.isNaN(value) ? value : EXCLUDED_PARAM);
+    if (
+      !Number.isNaN(value)
+      && value !== null
+      && value !== undefined
+    ) {
+      return value;
+    }
+    return EXCLUDED_PARAM;
   },
 
   isTruthy: (value) => {
