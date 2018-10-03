21d13f571a4d8feab480912159870d688a11ef2f
Gabe Abrams <gabeabrams@gmail.com>
2018-10-03 15:58:08 -0400

    Added reference to new endpoints

--- endpoints/config.js
+++ endpoints/config.js
@@ -4,11 +4,16 @@ const courseAssignmentGroups = require('./course/assignmentGroups.js');
 const courseAssignments = require('./course/assignments.js');
 const courseCourse = require('./course/course.js');
 const courseEnrollments = require('./course/enrollments.js');
+const courseGradebookColumns = require('./course/gradebookColumns.js');
+const courseGroups = require('./course/groups.js');
+const courseGroupSets = require('./course/groupSets.js');
 const coursePages = require('./course/pages.js');
+const courseQuizzes = require('./course/quizzes.js');
 const courseSections = require('./course/sections.js');
 
 // User
 const userCourse = require('./user/course.js');
+const userSelf = require('./user/self.js');
 
 module.exports = {
   course: [
@@ -17,10 +22,15 @@ module.exports = {
     courseAssignments,
     courseCourse,
     courseEnrollments,
+    courseGradebookColumns,
+    courseGroups,
+    courseGroupSets,
     coursePages,
+    courseQuizzes,
     courseSections,
   ],
   user: [
     userCourse,
+    userSelf,
   ],
 };
