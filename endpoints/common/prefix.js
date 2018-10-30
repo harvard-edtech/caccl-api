761d0259a0a55506f317f43123073a7b68a9c6b3
Gabe Abrams <gabeabrams@gmail.com>
2018-10-30 15:10:31 -0400

    Removed endpoints

--- endpoints/common/prefix.js
+++
@@ -1,9 +0,0 @@
-/**
- * Prefixes for each version of the Canvas api
- * @module endpoints/helpers/prefix
- * @see module: endpoints/helpers/prefix
- */
-
-module.exports = {
-  v1: '/api/v1',
-};
--- test/common/genInstructorAPI.js
+++
@@ -1,9 +0,0 @@
-const API = require('../../index.js');
-const environment = require('../environment.js');
-
-module.exports = () => {
-  return new API({
-    accessToken: environment.accessToken,
-    canvasHost: environment.canvasHost,
-  });
-};
