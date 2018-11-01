f6cfcfbdce8cd363bcc529a9910ae3d8961cb68d
Gabe Abrams <gabeabrams@gmail.com>
2018-11-01 13:37:55 -0400

    Switched to axios as network sender

--- classes/visitEndpoint/helpers/defaultSendRequest.js
+++ classes/visitEndpoint/helpers/defaultSendRequest.js
@@ -1,66 +1,72 @@
-const querystring = require('querystring');
-const https = require('https');
+const axios = require('axios');
+const qs = require('qs');
 const CACCLError = require('../../../../caccl-error/index.js'); // TODO: switch to actual library
 const errorCodes = require('../../../errorCodes.js');
+//
+// const paramsSerializer = (params) => {
+//   const ret = queryString.stringify(params, { arrayFormat: 'bracket' });
+//   console.log(ret);
+//   return ret;
+// };
 
 // Function that sends (and retries) an https request
-function sendRequest(options) {
-  return new Promise((resolve, reject) => {
-    // Set max number of retries if not defined
-    const numRetries = (options.numRetries ? options.numRetries : 0);
+const sendRequest = (options) => {
+  // Set max number of retries if not defined
+  const numRetries = (options.numRetries ? options.numRetries : 0);
 
-    // Encode content
-    const content = querystring.stringify(options.params || {});
-    const request = {
-      host: options.host,
-      path: options.path,
-      method: options.method || 'GET',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': content.length,
-      },
-    };
+  // Use default method if applicable
+  const method = options.method || 'GET';
 
-    // Send request
-    const req = https.request(request, (res) => {
-      let text = '';
-
-      // Save a chunk of the response
-      res.on('data', (chunk) => {
-        text += chunk;
-      });
-
-      // Upon finishing, resolve with full response
-      res.on('end', () => {
-        resolve({
-          body: text,
-          status: res.statusCode,
-          headers: res.headers,
-        });
-      });
-    });
-
-    // Try again if we received an error
-    req.on('error', () => {
-      if (numRetries > 0) {
-        const newOptions = options;
-        newOptions.numRetries = numRetries - 1;
-        sendRequest(newOptions).then((results) => {
-          resolve(results);
-        });
-      } else {
-        // No tries left
-        return reject(new CACCLError({
-          message: 'We could not connect with Canvas. This happens when Canvas is down or when Canvas throttles requests. If this issue persists, contact an admin.',
-          code: errorCodes.notConnected,
-        }));
-      }
-    });
-
-    // Write and close connection
-    req.write(content);
-    req.end();
+  // Stringify parameters
+  const stringifiedParams = qs.stringify(options.params || {}, {
+    encodeValuesOnly: true,
+    arrayFormat: 'brackets',
   });
-}
 
+  // Create url (include query if GET)
+  const query = (method === 'GET' ? `?${stringifiedParams}` : '');
+  const url = `https://${options.host}${options.path}${query}`;
+
+  // Create data (only if not GET)
+  const data = (method !== 'GET' ? stringifiedParams : null);
+
+  // Send request
+  return axios({
+    method,
+    url,
+    data,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+    .catch((err) => {
+      // Axios throws an error if the request status indicates an error
+      // sendRequest is supposed to resolve if the request went through, whether
+      // the status indicates an error or not.
+      if (err.response) {
+        // Resolve with response
+        return Promise.resolve(err.response);
+      }
+      // Request failed! Check if we have more attempts
+      if (numRetries > 0) {
+        // Update options with one less retry
+        const newOptions = options;
+        newOptions.numRetries -= 1;
+        return sendRequest(newOptions);
+      }
+
+      // No tries left
+      throw new CACCLError({
+        message: 'We encountered an error when trying to send a network request. If this issue persists, contact an admin.',
+        code: errorCodes.notConnected,
+      });
+    })
+    .then((response) => {
+      return {
+        body: response.data,
+        status: response.status,
+        headers: response.headers,
+      };
+    });
+};
 module.exports = sendRequest;
