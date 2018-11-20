3c05c47a4bf83b3f3462a61d996236d33c348187
Gabe Abrams <gabeabrams@gmail.com>
2018-11-20 11:29:18 -0500

    Allowing config passthru so you can customize the api settings

--- test/common/genInstructorAPI.js
+++ test/common/genInstructorAPI.js
@@ -1,9 +1,9 @@
 const API = require('../../index.js');
 const environment = require('../environment.js');
 
-module.exports = () => {
-  return new API({
-    accessToken: environment.accessToken,
-    canvasHost: environment.canvasHost,
-  });
+module.exports = (config = {}) => {
+  const newConfig = config;
+  newConfig.accessToken = environment.accessToken;
+  newConfig.canvasHost = environment.canvasHost;
+  return new API(newConfig);
 };
