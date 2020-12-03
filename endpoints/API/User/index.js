280aa68c71b8000c5555ad23bf0f0cf651b95601
Gabe Abrams <gabeabrams@gmail.com>
2020-12-03 15:36:57 -0500

    Added user.search function

--- endpoints/API/User/index.js
+++ endpoints/API/User/index.js
@@ -91,6 +91,73 @@ User.listEmails.scopes = [
   'url:GET|/api/v1/users/:user_id/communication_channels',
 ];
 
+/**
+ * Search a user
+ * @author Gabe Abrams
+ * @memberof api.user
+ * @instance
+ * @async
+ * @method search
+ * @param {object} options - object containing all arguments
+ * @param {number} options.accountId - the account to search through
+ * @param {string} options.searchTerm - a search term to apply (must be at
+ *   least 3 chars). Can be a full ID or partial name. For admins, searches
+ *   SIS ID, login ID, name, and email address.
+ * @param {boolean} [options.isStudent] - if true, only search for students.
+ *   Only one user type boolean can be true
+ * @param {boolean} [options.isTeacher] - if true, only search for teachers.
+ *   Only one user type boolean can be true
+ * @param {boolean} [options.isTA] - if true, only search for TAs.
+ *   Only one user type boolean can be true
+ * @param {boolean} [options.isObserver] - if true, only search for observers.
+ *   Only one user type boolean can be true
+ * @param {boolean} [options.isDesigner] - if true, only search for designers.
+ *   Only one user type boolean can be true
+ * @param {string} [options.sortBy=username] - the item to sort by. Can be:
+ *   "username" or "email" or "sis_id" or "last_login"
+ * @param {boolean} [options.sortDescending] - if true, sort descending
+ * @return {object[]} list of user objects {@link https://canvas.instructure.com/doc/api/users.html#User}
+ */
+User.search = function (options) {
+  // Get search term
+  const searchTerm = String(options.searchTerm);
+
+  // Figure out enrollment type
+  let enrollmentType;
+  if (options.isStudent) {
+    enrollmentType = 'student';
+  } else if (options.isTeacher) {
+    enrollmentType = 'teacher';
+  } else if (options.isTA) {
+    enrollmentType = 'ta';
+  } else if (options.isObserver) {
+    enrollmentType = 'observer';
+  } else if (options.isDesigner) {
+    enrollmentType = 'designer';
+  }
+
+  // Figure out other parameters
+  const sort = options.sortBy || 'username';
+  const order = (options.sortDescending ? 'desc' : 'asc');
+
+  // Send request
+  return this.visitEndpoint({
+    path: `${prefix.v1}/accounts/${options.accountId}/users`,
+    method: 'GET',
+    params: {
+      sort,
+      order,
+      search_term: searchTerm,
+      enrollment_type: enrollmentType,
+    },
+  });
+};
+User.search.action = 'search for a user or a list of users';
+User.search.requiredParams = ['accountId', 'searchTerm'];
+User.search.scopes = [
+  'url:GET|/api/v1/accounts/:account_id/users',
+];
+
 /*------------------------------------------------------------------------*/
 /*                                 Export                                 */
 /*------------------------------------------------------------------------*/
